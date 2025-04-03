d0022851c106fbe5526d4f1909bf28ef76e7db90
EC2 Default User <ec2-user@ip-172-31-43-29.ap-northeast-1.compute.internal>
2025-04-03 14:08:08 +0000

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to:"homes#top"
+  get 'homes/about', to: 'homes#about', as: :about
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :favorite, only: [:create,:destroy]
+    resources :post_comments, only: [:create,:destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+end
